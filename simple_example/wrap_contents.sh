07904b45e237efb3b3b56ec6a126c07f971affde
Nick Plucker <pluck011@morris.umn.edu>
2018-09-05 11:48:03 -0500

    Add my_chart.html file produced from concatentation script

--- simple_example/wrap_contents.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-wrapped=$1
-header=$2_header.html
-footer=$2_footer.html
-target=$3
-
-# Concatenate the header, the middle and the footer and output to the target file
-cat $header $wrapped $footer | uniq > $target
